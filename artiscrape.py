edb9a25c560ddf8ed2f457524d10e87f44807d86
Mudb0y <stasprzecinek@gmail.com>
2022-07-25 16:05:45 +0200

    Clean all code in preparation for rewrite.

--- artiscrape.py
+++ artiscrape.py
@@ -4,15 +4,3 @@ from time import sleep
 import requests
 import bs4 as bs
 
-# Send a request to fanfiction.net/s/1 through fanfiction.net/s/15000000. If one of the pages contains the string "Artemis Fowl fanfic" in the title of the page continue.
-for i in range(1, 15000000):
-    url = f"https://www.fanfiction.net/s/{i}"
-    response = requests.get(url)
-    soup = bs.BeautifulSoup(response.text, "html.parser")
-    print ("Checking " +url)
-    sleep (1)
-title = soup.find("h1", {"class": "title"})
-if title.text.find("Artemis Fowl fanfic") != -1:
-    # Download the fic using fanficfare.
-    print ("Downloading from " +url)
-os.system ("fanficfare -f txt --non-interactive " +url)
