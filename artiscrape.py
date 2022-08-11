2738b0311e8a1e2b9a736b4d9f07a6107bf7e739
Stanislaw Przedzinkowski <48333171+Mudb0y@users.noreply.github.com>
2022-08-11 19:29:24 +0200

    Make a basic URL lookup.

--- artiscrape.py
+++ artiscrape.py
@@ -4,3 +4,14 @@ from time import sleep
 import requests
 import bs4 as bs
 
+for i in range(1, 40882393):
+    url = "https://archiveofourown.org/works/" + str(i)
+    response = requests.get(url)
+    soup = bs.BeautifulSoup(response.text, "html.parser")
+    if soup.title.text == "Artemis Fowl - Eoin Colfer":
+        with open("valid-urls.txt", "a") as f:
+            f.write(url + "\n")
+        print ("Valid URL found.")
+    else:
+        pass
+    sleep(1)
